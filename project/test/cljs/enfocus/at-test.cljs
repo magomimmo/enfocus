657d7aab83783f6448c7dd871206f5da04912573
Magomimmo <mimmo.cosenza@gmail.com>
2013-10-01 14:33:34 +0200

    added test

---
+++ project/test/cljs/enfocus/at-test.cljs
@@ -0,0 +1,11 @@
+(ns enfocus.at-test
+  (:require-macros [cemerick.cljs.test :as tm]
+                   [enfocus.macros :as em]
+                   [hiccups.core :as hi])
+  (:require [cemerick.cljs.test :as t]
+            [enfocus.core :as ef]
+            [hiccups.runtime :as hr]))
+
+
+
+
