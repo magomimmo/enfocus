98809adf2a9394c8f8ce5f76e549dcfd74651673
magomimmo <mimmo.cosenza@gmail.com>
2013-10-02 09:32:53 +0200

    update to cljx 0.3.0 and new directory layout

--- project/project.clj
+++ project/project.clj
@@ -1,24 +1,23 @@
 (defproject enfocus "2.0.0-SNAPSHOT"
   :description "DOM manipulation tool for clojurescript inspired by Enlive"
-  :source-paths ["cljs-src" ".generated/cljs" ".generated/clj"]
+  :source-paths ["src/clj" "target/clj"]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [domina "1.0.2-SNAPSHOT"]
                  [org.jsoup/jsoup "1.7.2"]
                  ]
   :plugins [[lein-cljsbuild "0.3.0"]
-            [com.keminglabs/cljx "0.2.2"]]
-  :cljx {:builds [{:source-paths ["cljx-src"]
-                   :output-path ".generated/clj"
-                   :rules cljx.rules/clj-rules}
+            [com.keminglabs/cljx "0.3.0"]]
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/clj"
+                   :rules :clj}
 
-                  {:source-paths ["cljx-src"]
-                   :output-path ".generated/cljs"
-                   :extension "cljs"
-                   :rules cljx.rules/cljs-rules}]}
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/cljs"
+                   :rules :cljs}]}
   :cljsbuild
   {:builds
    [{:builds nil,
-     :source-paths ["cljs-src" ".generated/cljs"]
+     :source-paths ["src/cljs" "target/cljs"]
      :compiler
      {:output-dir "../testing/resources/public/cljs",
       :output-to "../testing/resources/public/cljs/enfocus.js",
