df4ee6ad4cd50f434bbc5bbea9aa6d68e04dc431
magomimmo <mimmo.cosenza@gmail.com>
2013-10-02 09:48:14 +0200

    updated to crossover

--- project/project.clj
+++ project/project.clj
@@ -1,27 +1,20 @@
 (defproject enfocus "2.0.0-SNAPSHOT"
   :description "DOM manipulation tool for clojurescript inspired by Enlive"
-  :source-paths ["src/clj" "target/clj"]
+  :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [domina "1.0.2-SNAPSHOT"]
-                 [org.jsoup/jsoup "1.7.2"]
-                 ]
-  :plugins [[lein-cljsbuild "0.3.0"]
-            [com.keminglabs/cljx "0.3.0"]]
-  :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "target/clj"
-                   :rules :clj}
-
-                  {:source-paths ["src/cljx"]
-                   :output-path "target/cljs"
-                   :rules :cljs}]}
+                 [org.jsoup/jsoup "1.7.2"]]
+  :plugins [[lein-cljsbuild "0.3.0"]]
+  :hooks [leiningen.cljsbuild]
   :cljsbuild
-  {:builds
-   [{:builds nil,
-     :source-paths ["src/cljs" "target/cljs"]
+  {:crossover [enfocus.enlive.syntax]
+   :crossover-jar true
+   :builds
+   {:whitespace
+    {:source-paths ["src/cljs"]
      :compiler
-     {:output-dir "../testing/resources/public/cljs",
-      :output-to "../testing/resources/public/cljs/enfocus.js",
-      :optimizations :whitespace,
-      :pretty-print true}}]}
-  :hooks [cljx.hooks])
+     {:output-dir "../testing/resources/public/cljs"
+      :output-to "../testing/resources/public/cljs/enfocus.js"
+      :optimizations :whitespace
+      :pretty-print true}}}})
 
