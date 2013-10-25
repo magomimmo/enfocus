52bd4802a4b472412723b80bfd4a471d4a9e5a99
magomimmo <mimmo.cosenza@gmail.com>
2013-10-25 19:55:49 +0200

    added profiles.clj to keep the enfocus developer view separated from the enfocus user view

--- project.clj
+++ project.clj
@@ -8,8 +8,7 @@
   :min-lein-version "2.2.0"
 
   :source-paths ["src/clj"]
-  :test-paths ["test/clj"]
-
+  
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1847"]
                  [domina "1.0.2"]
@@ -29,48 +28,5 @@
               :compiler
               {:output-to "dev-resources/public/js/deploy.js"
                :optimizations :whitespace
-               :pretty-print true}}}}
-
-  :profiles {:dev {:resources-paths ["dev-resources"]
-
-                   :dependencies [[com.cemerick/piggieback "0.1.0"]
-                                  [ring "1.2.0"]
-                                  [compojure "1.1.5"]]
-                   :plugins [[com.cemerick/clojurescript.test "0.1.0"]]
-                   
-                   :cljsbuild 
-                   {:builds {:whitespace
-                             {:source-paths ["src/cljs" "test/cljs" "src/brepl"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/whitespace.js"
-                               :optimizations :whitespace
-                               :pretty-print true}}
-
-                             :simple
-                             {:source-paths ["src/cljs" "test/cljs"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/simple.js"
-                               :optimizations :simple
-                               :pretty-print false}}
-
-                             :advanced
-                             {:source-paths ["src/cljs" "test/cljs"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/advanced.js"
-                               :optimizations :advanced
-                               :pretty-print false}}}
-                    :test-commands {"whitespace"
-                                    ["phantomjs" :runner "dev-resources/public/js/whitespace.js"]
-                                    
-                                    "simple"
-                                    ["phantomjs" :runner "dev-resources/public/js/simple.js"]
-                                    
-                                    "advanced"
-                                    ["phantomjs" :runner "dev-resources/public/js/advanced.js"]}}
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-                   :injections [(require '[cljs.repl.browser :as brepl]
-                                         '[cemerick.piggieback :as pb])
-                                (defn browser-repl []
-                                  (pb/cljs-repl :repl-env
-                                                (brepl/repl-env :port 9000)))]}})
+               :pretty-print true}}}})
 
