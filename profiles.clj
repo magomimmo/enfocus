751b24329d3cd166b720f4c7782c4591c5961379
magomimmo <mimmo.cosenza@gmail.com>
2013-11-03 10:24:24 +0100

    added cljx pluging

--- profiles.clj
+++ profiles.clj
@@ -1,27 +1,38 @@
 {:dev {:resources-paths ["dev-resources"]
-       :test-paths ["test/clj" "test/cljs"]
+       :test-paths ["test/clj" "target/test/clj" "test/cljs" "target/test/cljs"]
+       
        :dependencies [[com.cemerick/piggieback "0.1.0"]
                       [ring "1.2.0"]
                       [compojure "1.1.5"]]
-       :plugins [[com.cemerick/clojurescript.test "0.1.0"]]
+       
+       :plugins [[com.cemerick/clojurescript.test "0.1.0"]
+                 [com.keminglabs/cljx "0.3.0"]]
+
+       :cljx {:builds [{:source-paths ["test/cljx"]
+                        :output-path "target/test/clj"
+                        :rules :clj}
+
+                       {:source-paths ["test/cljx"]
+                        :output-path "target/test/cljs"
+                        :rules :cljs}]}
 
        :cljsbuild
        {:builds {:whitespace
-                 {:source-paths ["src/cljs" "test/cljs" "src/brepl"]
+                 {:source-paths ["src/cljs" "test/cljs" "src/brepl" "target/test/cljs"]
                   :compiler
                   {:output-to "dev-resources/public/js/whitespace.js"
                    :optimizations :whitespace
                    :pretty-print true}}
                              
                  :simple
-                 {:source-paths ["src/cljs" "test/cljs"]
+                 {:source-paths ["src/cljs" "test/cljs" "target/test/cljs"]
                   :compiler
                   {:output-to "dev-resources/public/js/simple.js"
                    :optimizations :simple
                    :pretty-print false}}
                              
                  :advanced
-                 {:source-paths ["src/cljs" "test/cljs"]
+                 {:source-paths ["src/cljs" "test/cljs" "target/test/cljs"]
                   :compiler
                   {:output-to "dev-resources/public/js/advanced.js"
                    :optimizations :advanced
