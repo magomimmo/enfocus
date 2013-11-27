0b63046a8055d64828d9c1d8cfd7c773c8546a5e
magomimmo <mimmo.cosenza@gmail.com>
2013-11-27 08:49:12 +0100

    move dev profile to profiles.clj

--- project.clj
+++ project.clj
@@ -11,11 +11,11 @@
   
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1847"]
+                 [org.clojure/clojurescript "0.0-2069"]
                  [domina "1.0.2"]
                  [org.jsoup/jsoup "1.7.2"]]
 
-  :plugins [[lein-cljsbuild "0.3.4"]]
+  :plugins [[lein-cljsbuild "1.0.0"]]
   
   :hooks [leiningen.cljsbuild]
 
@@ -29,48 +29,5 @@
               :compiler
               {:output-to "dev-resources/public/js/deploy.js"
                :optimizations :none
-               :pretty-print false}}}}
-
-  :profiles {:dev {:resources-paths ["dev-resources"]
-                   :test-paths ["test/clj" "test/cljs"]
-                   :dependencies [[com.cemerick/piggieback "0.1.0"]
-                                  [ring "1.2.0"]
-                                  [compojure "1.1.5"]]
-                   :plugins [[com.cemerick/clojurescript.test "0.1.0"]]
-
-                   :cljsbuild
-                   {:builds {:whitespace
-                             {:source-paths ["src/cljs" "test/cljs" "src/brepl"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/whitespace.js"
-                               :optimizations :whitespace
-                               :pretty-print true}}
-                             
-                             :simple
-                             {:source-paths ["src/cljs" "test/cljs"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/simple.js"
-                               :optimizations :simple
-                               :pretty-print false}}
-                             
-                             :advanced
-                             {:source-paths ["src/cljs" "test/cljs"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/advanced.js"
-                               :optimizations :advanced
-                               :pretty-print false}}}
-                    :test-commands {"whitespace"
-                                    ["phantomjs" :runner "dev-resources/public/js/whitespace.js"]
-
-                                    "simple"
-                                    ["phantomjs" :runner "dev-resources/public/js/simple.js"]
-
-                                    "advanced"
-                                    ["phantomjs" :runner "dev-resources/public/js/advanced.js"]}}
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-                   :injections [(require '[cljs.repl.browser :as brepl]
-                                         '[cemerick.piggieback :as pb])
-                                (defn browser-repl []
-                                  (pb/cljs-repl :repl-env
-                                                (brepl/repl-env :port 9000)))]}})
+               :pretty-print false}}}})
 
